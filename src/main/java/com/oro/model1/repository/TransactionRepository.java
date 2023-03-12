438ea73f6b52fdb7271ce311553d57d4bbb739eb
szymonaszek01 <235879@edu.p.lodz.pl>
2023-03-12 21:03:04 +0100

    task-002-adding-repositories

---
+++ src/main/java/com/oro/model1/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.oro.model1.repository;
+
+import com.oro.model1.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
